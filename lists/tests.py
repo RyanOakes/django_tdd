17ac00a40931bb06b12bdd24383d2b39bc9199b9
oakes <oakesonline@gmail.com>
2017-08-16 15:20:55 -0400

    Created app urls, added first unit-test, edited Jenkins file

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,21 @@
+from lists.views import home_page
+from django.urls import resolve
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        '''
+        resolve is the function Django uses internally to resolve URLs and find
+        what view function they should map to.  We are checking that resolve, when
+        called with “/”, the root of the site, finds a function called home_page.
+        '''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+
+# class SmokeTest(TestCase):
+#
+#     def test_bad_maths(self):
+#         self.assertEqual(1 + 1, 3)
